635eb178c64c7b329eb0fdeffbea9588f0cffeb8
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-08-06 10:57:11 +0700

    Update ___FILEBASENAME___Router.swift

--- VIPPER.xctemplate/___FILEBASENAME___Router.swift
+++ VIPPER.xctemplate/___FILEBASENAME___Router.swift
@@ -1,7 +1,7 @@
 //___FILEHEADER___
 
 import UIKit
-import Resolver
+import MPInjector
 
 protocol ___FILEBASENAMEASIDENTIFIER___Interface {
     var view: ___VARIABLE_productName___ViewInterface? { get }
@@ -24,10 +24,10 @@ final class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Interf
 
 }
 
-extension ___FILEBASENAMEASIDENTIFIER___: ResolverRegistering {
+extension ___FILEBASENAMEASIDENTIFIER___ {
     static func registerAllServices() {
-        Resolver.register { ___VARIABLE_productName___Interactor() as ___VARIABLE_productName___InteractorInterface }
-        Resolver.register { ___VARIABLE_productName___Router() as ___VARIABLE_productName___RouterInterface }
-        Resolver.register { ___VARIABLE_productName___Presenter() as ___VARIABLE_productName___PresenterInterface }
+        MPInjector.register { ___VARIABLE_productName___Interactor() as ___VARIABLE_productName___InteractorInterface }
+        MPInjector.register { ___VARIABLE_productName___Router() as ___VARIABLE_productName___RouterInterface }
+        MPInjector.register { ___VARIABLE_productName___Presenter() as ___VARIABLE_productName___PresenterInterface }
     }
 }
